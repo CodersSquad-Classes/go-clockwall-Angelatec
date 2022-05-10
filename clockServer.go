bbfd8a6c6bc45b3fc0e08e3bf76fdba173cfdf3d
angela7744 <58756436+angela7744@users.noreply.github.com>
2022-05-10 17:11:27 -0500

    ClockWall

--- clockServer.go
+++ clockServer.go
@@ -2,16 +2,29 @@
 package main
 
 import (
+	"fmt"
 	"io"
 	"log"
 	"net"
+	"os"
 	"time"
 )
 
-func handleConn(c net.Conn) {
+func TimeIn(t time.Time, name string) (time.Time, error) {
+	loc, err := time.LoadLocation(name)
+	if err == nil {
+		t = t.In(loc)
+	}
+	return t, err
+}
+func handleConn(c net.Conn, name string) {
 	defer c.Close()
 	for {
-		_, err := io.WriteString(c, time.Now().Format("15:04:05\n"))
+		t, erro := TimeIn(time.Now(), name)
+		if erro != nil {
+			return // e.g., client disconnected
+		}
+		_, err := io.WriteString(c, name+" :"+t.Format("15:04:05\n"))
 		if err != nil {
 			return // e.g., client disconnected
 		}
@@ -20,7 +33,14 @@ func handleConn(c net.Conn) {
 }
 
 func main() {
-	listener, err := net.Listen("tcp", "localhost:9090")
+	if len(os.Args) < 2 {
+		fmt.Println("Missing Parameters")
+		os.Exit(-1)
+	}
+	name := os.Getenv("TZ")
+	fmt.Println("Aux", name)
+	port := "localhost:" + os.Args[2]
+	listener, err := net.Listen("tcp", port)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -30,6 +50,6 @@ func main() {
 			log.Print(err) // e.g., connection aborted
 			continue
 		}
-		go handleConn(conn) // handle connections concurrently
+		go handleConn(conn, name) // handle connections concurrently
 	}
 }
--- clockWall.go
+++ clockWall.go
@@ -1,5 +1,46 @@
 package main
 
+import (
+	"fmt"
+	"io"
+	"log"
+	"net"
+	"os"
+)
+
+func mustCopy(dst io.Writer, src io.Reader) {
+	if _, err := io.Copy(dst, src); err != nil {
+		log.Fatal(err)
+	}
+}
+
+/*func outClock(conn net.Conn) {
+	defer conn.Close()
+	mustCopy(os.Stdout, conn)
+}*/
+
 func main() {
-	// Let's start the fun
+	ch := make(chan int, 3)
+
+	for i := 1; i <= len(os.Args)-1; i++ {
+		input := os.Args[i]
+		for j := 0; j <= len(input)-1; j++ {
+			if input[j:j+1] == "=" {
+				go func() {
+					fmt.Printf("jasda", input[j+1:len(input)])
+					conn, err := net.Dial("tcp", input[j+1:len(input)])
+					if err != nil {
+						fmt.Println("f")
+						log.Fatal(err)
+					}
+					defer conn.Close()
+					mustCopy(os.Stdout, conn)
+					ch <- 2
+				}()
+				break
+			}
+		}
+
+	}
+	<-ch
 }
